e60312498749a487f6f87b1fdf884a15c3b26a1f
SoulDevt <souleymane.gueye.pro@gmail.com>
2023-03-25 11:12:35 +0100

    clean profileview

--- front/src/views/ProfileView.vue
+++ front/src/views/ProfileView.vue
@@ -1,101 +1,69 @@
 <template>
-
-    <form @submit.prevent="editUser()" class="flex flex-col gap-5">
+  <div  class="flex flex-col gap-5">
     <div class="overflow-hidden bg-white shadow sm:rounded-lg">
-  <div class="px-4 py-5 sm:px-6">
-    <h3 class="text-lg font-medium leading-6 text-gray-900">Profile</h3>
-    <p class="mt-1 max-w-2xl text-sm text-gray-500">Profile information</p>
-  </div>
-  <div class="border-t border-gray-200">
-    <dl>
-      <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-500">Full name</dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{name}}</dd>
+      <div class="px-4 py-5 sm:px-6">
+        <h3 class="text-lg font-medium leading-6 text-gray-900">Profile</h3>
+        <p class="mt-1 max-w-2xl text-sm text-gray-500">Profile information</p>
       </div>
-
-      <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-500">Email address</dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{email}}</dd>
-      </div>
-      <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-500">Role</dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ roles }}</dd>
-      </div>
-
-
-    </dl>
-  </div>
-  <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
-            <button type="submit" class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Modifier</button>
+      <div class="border-t border-gray-200">
+        <dl>
+          <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">Full name</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{name}}</dd>
           </div>
-</div>
-</form>
+
+          <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">Email address</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{email}}</dd>
+          </div>
+          <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">Role</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ roles }}</dd>
+          </div>
+        </dl>
+      </div>
+      <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+        <RouterLink to="/update-user">
+          <button type="button" class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Modifier</button>
+        </RouterLink>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import { useRouter } from "vue-router";
 import { useUserStore } from "@/store/user";
 import { useToast } from "vue-toastification";
-import { ENTRYPOINT } from "../../config/entrypoint";
-import { ref } from "vue";
+import { ref,onBeforeMount } from "vue";
 import { useCookies } from "@vueuse/integrations/useCookies";
+
+
 const cookies = useCookies();
-
-console.log(cookies.get("token"))
-
-
 const userStore = useUserStore();
 const toast = useToast();
 const userId = JSON.parse(localStorage.getItem('user')).id
-console.log(userId)
 const email = ref(null)
-console.log(email._value)
 const name = ref(null)
 const roles = ref(null)
 const idUser = ref(null)
 const router = useRouter();
-console.log("all:" + email.value,name.value)
-
-// const getUser = async () => {
-//         const response = await fetch(`${ENTRYPOINT}/users/${userId}`, {
-//         method: "GET",
-//         headers: {
-//           Authorization: `Bearer ${cookies.get("token")}`,
-//             "Content-Type": "application/json",
-//             Accept: "application/json",
-//         },
-//         }).then(res => res.json());
-//         if (response) {
-//             console.log(response)
-//             email.value = response.email;
-//             name.value = response.username;
-//             if(response.roles = "['ROLE_USER']") {
-//                 roles.value = "User"
-//             }
-
-//         } else {
-//             throw new Error("Erreur");
-//         }
-//     };
-
-    const getUser = async () => {
-      const response = await userStore.getUser();
-      if(response) {
-        email.value = response.email;
-        name.value = response.username;
-        if(response.roles = "['ROLE_USER']") {
-            roles.value = "User"
-        }
-      } else {
-        throw new Error("Erreur");
-      }
+const getUser = async () => {
+  const response = await userStore.getUser();
+  if(response) {
+    email.value = response.email;
+    name.value = response.username;
+    if(response.roles = "['ROLE_USER']") {
+        roles.value = "User"
     }
-
-getUser();
-
-const editUser = async () => {
-  await router.push("/update-user");
+  } else {
+    throw new Error("Erreur");
+  }
 }
+
+onBeforeMount(() => {
+  getUser();
+})
     
 
 </script>
