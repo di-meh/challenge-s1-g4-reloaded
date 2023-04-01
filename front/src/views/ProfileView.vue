7b946483a5dc5e42bdfe95384a1bf3b28425ba68
SoulDevt <souleymane.gueye.pro@gmail.com>
2023-04-01 10:36:32 +0200

    adjust design profile

--- front/src/views/ProfileView.vue
+++ front/src/views/ProfileView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div  class="flex flex-col gap-5">
+  <div  class="flex flex-col gap-5" style="width:45%;margin: 0 auto;margin-top:5%;">
     <div class="overflow-hidden bg-white shadow sm:rounded-lg">
       <div class="px-4 py-5 sm:px-6">
         <h3 class="text-lg font-medium leading-6 text-gray-900">Profile</h3>
@@ -7,14 +7,15 @@
       </div>
       <div class="border-t border-gray-200">
         <dl>
-          <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt class="text-sm font-medium text-gray-500">Username</dt>
+          <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6" >
+            <dt class="text-sm font-medium text-gray-500">Full name</dt>
             <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{name}}</dd>
           </div>
           <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt class="text-sm font-medium text-gray-500">Email address</dt>
             <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{email}}</dd>
           </div>
+
         </dl>
       </div>
       <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
@@ -48,6 +49,7 @@ const getUser = async () => {
   if(response) {
     email.value = response.email;
     name.value = response.username;
+
 
   } else {
     throw new Error("Erreur");
