8717581053571baffe170c561afcf41bf51aff0e
nikola panic <npanic@myges.fr>
2023-04-15 12:42:07 +0200

    add color to annonces

--- front/src/views/AnnonceView.vue
+++ front/src/views/AnnonceView.vue
@@ -1,8 +1,8 @@
 <template>
         <p v-if="items.length <= 0">Aucunes annonces !</p>
-        <div class="bg-white">
+        <div class="text-white">
             <div class="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-              <h2 class="text-xl font-bold text-gray-900">Annonces</h2>
+              <h2 class="text-xl font-bold">Annonces</h2>
         
               <div class="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
                 
