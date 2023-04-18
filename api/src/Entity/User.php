b2a0ec30642e0f5324dc09bf1c7b0ce612988deb
francisco.leblanc.sacha <francisco.leblanc.sacha@gmail.com>
2023-04-18 11:24:04 +0200

    euh

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -16,7 +16,6 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
-use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
